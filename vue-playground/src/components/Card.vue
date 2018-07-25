3326c59d2691bc00c9eb34149bdd6556d06c17af
tom lee <tdroid99@gmail.com>
2018-07-25 07:23:22 -0600

    working on practice

---
+++ vue-playground/src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="drimg">
+        <h2>I'M {{dragon.name}}</h2>
+        <h3>My ID is {{dragon.id}}</h3>
+        <img class="dragpic" :src='dragon.imageUrl'>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: "Card",
+    props: ['dragon']
+}
+</script>
+
+<style>
+.drimg {
+    border: 2px;
+    border-color: red;
+    background-color: aqua;
+}
+.dragpic {
+    height: 200px;
+    width: 200px;
+}
+</style>
---
+++ vue-playground/src/components/CardList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <Card v-for="dragon in dragons" :key='dragon.id' :dragon="dragon"/>
+    </div>
+</template>
+
+<script>
+import Card from './Card'
+export default {
+    name: "CardList",
+    components: { 
+        Card
+        },
+data(){
+    return {
+        dragons: [  {name: "Tiamat", id: 1, imageUrl:'https://www.sciencenewsforstudents.org/sites/default/files/scald-image/860-dragon-header-iStock-494839519.gif'},
+                    {name: "Smaug", id: 2, imageUrl:'https://vignette.wikia.nocookie.net/reddits-world/images/9/9a/RedDragon.jpg/revision/latest?cb=20140914212250'},
+                    {name: "Puff", id:3, imageUrl:'https://www.sciencenewsforstudents.org/sites/default/files/scald-image/860-dragon-header-iStock-494839519.gif'},
+                    {name: "Piro", id: 4, imageUrl:'https://i.ytimg.com/vi/P3SgpYXxqfs/maxresdefault.jpg'}
+                ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
